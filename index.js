1df81e44c2fa1bbebc5914f8a6a41bf34d844a1c
Chris <Siege.Hatch@gmail.com>
2023-12-28 08:52:37 -0600

    parse input, parse data, process commands

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 import { createWebSocketStream, WebSocket, WebSocketServer } from "ws";
+import { Transform } from "stream";
 
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data) {
-    console.log(`Server received: ${data}`);
-    // ws.send("Awesome! Good job, buddy!");
+const parseInputTransformBuilder = (label, ws) =>
+  new Transform({
+    transform(chunk, encoding, next) {
+      const string = chunk.toString().trim();
+      if (string[0] === ":") {
+        if (string === ":close") {
+          ws.close();
+        }
+      } else this.push(chunk);
+      next();
+    },
   });
 
+const parseDataTransformBuilder = (label) =>
+  new Transform({
+    transform(chunk, encoding, next) {
+      const string = chunk.toString();
+      this.push(`${label} received: ${string}`);
+      next();
+    },
+  });
+
+const wsServer = new WebSocketServer({ port: 8080 });
+
+wsServer.on("connection", function connection(ws) {
+  const wsTransform = createWebSocketStream(ws, { encoding: "utf8" });
+  wsTransform.pipe(parseDataTransformBuilder("Server")).pipe(process.stdout);
+  process.stdin
+    .pipe(parseInputTransformBuilder("Server", wsServer))
+    .pipe(wsTransform);
+
   console.log("Server connected.");
+});
+
+wsServer.on("close", function close() {
+  console.log("Server disconnected.");
+  process.stdin.unpipe();
 });
 
 const wsClient = new WebSocket("ws://localhost:8080");
@@ -17,16 +46,18 @@ wsClient.on("error", console.error);
 
 wsClient.on("open", function open() {
   console.log("Client connected.");
-  //   wsClient.send("I'm connected!");
 });
 
-wsClient.on("message", function message(data) {
-  console.log("Client received: %s", data);
+wsClient.on("close", function close() {
+  console.log("Client disconnected.");
 });
 
-const duplex = createWebSocketStream(wsClient, { encoding: "utf8" });
+const clientTransform = createWebSocketStream(wsClient, { encoding: "utf8" });
+clientTransform.on("close", function close() {
+  console.log("Client duplex disconnected.");
+});
 
-duplex.on("error", console.error);
-
-duplex.pipe(process.stdout);
-process.stdin.pipe(duplex);
+clientTransform.pipe(parseDataTransformBuilder("Client")).pipe(process.stdout);
+process.stdin
+  .pipe(parseInputTransformBuilder("Client", wsClient))
+  .pipe(clientTransform);
