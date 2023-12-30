410fed4e8d4378c1ec3f75017ae77355e699af83
Chris <Siege.Hatch@gmail.com>
2023-12-30 08:34:26 -0600

    If connection closes while client waiting remove from pool

--- server.js
+++ server.js
@@ -5,28 +5,37 @@ import { teardown } from "./teardown.js";
 import { buildMessage } from "./utils/index.js";
 
 const wsServer = new WebSocketServer({ port: 8080 });
-const waitingClients = [];
+const waitingClients = {};
 let id = 0;
 
 wsServer.on("connection", (ws) => {
+  const wsId = id++;
   ws.on("error", console.error);
   ws.on("close", () => {
+    delete waitingClients[wsId];
     console.log("Connection closed.");
   });
 
   const wsStream = createWebSocketStream(ws, { encoding: "utf8" });
 
-  if (waitingClients.length > 0) {
-    const client = waitingClients.shift();
+  const waitingClientKeys = Object.keys(waitingClients);
+
+  if (waitingClientKeys.length > 0) {
+    const clientKey = waitingClientKeys[0];
+    const client = waitingClients[clientKey];
+
+    delete waitingClients[clientKey];
 
     client.pipe(wsStream);
     wsStream.pipe(client);
     client.write(
       buildMessage("text", "Another player has joined!\n", "Server")
     );
-    wsStream.write(buildMessage("text", "You have joined!\n", "Server"));
+    wsStream.write(
+      buildMessage("text", "You have joined another player!\n", "Server")
+    );
   } else {
-    waitingClients.push(wsStream);
+    waitingClients[wsId] = wsStream;
     wsStream.write(
       buildMessage("text", "Waiting for another player...", "Server")
     );
