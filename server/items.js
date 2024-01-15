c24c30556f1537783e408e0df20f4dab647cb1b0
Chris <Siege.Hatch@gmail.com>
2024-01-15 11:58:20 -0600

    The northern road, chapel, and key

--- server/items.js
+++ server/items.js
@@ -10,8 +10,9 @@ const canteen = buildItem(
   "A canteen filled with water. It's heavy."
 );
 const pewterMug = buildItem("mug", "A pewter mug. It's empty.");
+const key = buildItem("key", "A small iron key.");
 
-export const items = [canteen, pewterMug].reduce((acc, item) => {
+export const items = [canteen, pewterMug, key].reduce((acc, item) => {
   acc[item.id] = item;
   acc[item.name] = item;
   return acc;
--- server/map.js
+++ server/map.js
@@ -42,6 +42,36 @@ The air is thick with smoke and the smell of cheap ale.
 A few patrons sit at the bar.
 The door to the east leads back to the town square.`);
 
+const northernRoad1 = buildRoom(
+  `You are on a road leading north into the Whispering Fen.
+You hear the sounds of the fen all around you. It is unnerving.
+The road continues to the north. Mistfall Hollow lies to the south.`
+);
+
+const northernRoad2 = buildRoom(`You are on a road in the Whispering Fen.
+Beasts sound close; you can hear them moving in the shadows.
+The stench of rotting vegetation is overwhelming.
+The road bends eastward, but a small path leads north. To the south the road leads back toward Mistfall Hollow.`);
+
+const deadEnd = buildRoom(`You are on a small path in the Whispering Fen.
+The path ends in a bog with a wagon sunken into the mud.
+The cypress trees loom overhead. The air is thick with mist.
+The only way out is to the south.`);
+
+const chapelClearing =
+  buildRoom(`The road opens into a soggy clearing choked with thorns and briars.
+Overrun with vegetation, a ruined chapel stands in the center of the clearing.
+It is altogether too quiet here. Moving further east you think you might be able to enter the old chapel.
+The muddy road leads back to the west.`);
+
+const chapel = buildRoom(
+  `You clamber through a broken window into the ruined chapel.
+Rotting pews and a collapsed roof are all that remain.
+The windows are broken and the floor is covered in moss.
+The only way out is to the west.`,
+  [{ item: items["key"], quantity: 1 }]
+);
+
 townSquare.exits.north = northEdgeOfTown;
 northEdgeOfTown.exits.south = townSquare;
 townSquare.exits.east = eastEdgeOfTown;
@@ -51,12 +81,29 @@ itemShop.exits.north = townSquare;
 townSquare.exits.west = inn;
 inn.exits.east = townSquare;
 
+// the northern road
+northEdgeOfTown.exits.north = northernRoad1;
+northernRoad1.exits.south = northEdgeOfTown;
+northernRoad1.exits.north = northernRoad2;
+northernRoad2.exits.south = northernRoad1;
+northernRoad2.exits.north = deadEnd;
+deadEnd.exits.south = northernRoad2;
+northernRoad2.exits.east = chapelClearing;
+chapelClearing.exits.west = northernRoad2;
+chapelClearing.exits.east = chapel;
+chapel.exits.west = chapelClearing;
+
 export const map = [
   townSquare,
   northEdgeOfTown,
   eastEdgeOfTown,
   itemShop,
   inn,
+  northernRoad1,
+  northernRoad2,
+  deadEnd,
+  chapelClearing,
+  chapel,
 ].reduce((acc, room) => {
   acc[room.id] = room;
   return acc;
