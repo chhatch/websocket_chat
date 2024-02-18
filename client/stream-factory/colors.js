e068f1423970623094ed9a13e1320840babfbd15
Joe Elliott <jelliott.rrt@gmail.com>
2024-02-18 12:38:10 -0600

    colors.js
    in the hope to declutter my own hacks i have birthed colors.js

---
+++ client/stream-factory/colors.js
@@ -0,0 +1,64 @@
+// starforged industries
+// a cult of the final call
+// celebrating an impressive "30% uptime" 
+// Joe Elliott
+
+// used RGBA so we can change opacity if ever it seems like a cool idea
+
+const colors = {
+    red: "rgba(255, 0, 0, 1)",
+    green: "rgba(0, 128, 0, 1)",
+    blue: "rgba(0, 0, 255, 1)",
+    yellow: "rgba(255, 255, 0, 1)",
+    purple: "rgba(128, 0, 128, 1)",
+    teal: "rgba(0, 128, 128, 1)",
+    lime: "rgba(0, 255, 0, 1)",
+    pink: "rgba(255, 192, 203, 1)",
+    navy: "rgba(0, 0, 128, 1)",
+    maroon: "rgba(128, 0, 0, 1)",
+    olive: "rgba(128, 128, 0, 1)",
+    coral: "rgba(255, 127, 80, 1)",
+    aqua: "rgba(0, 255, 255, 1)",
+    magenta: "rgba(255, 0, 255, 1)",
+    gold: "rgba(255, 215, 0, 1)",
+    lavender: "rgba(230, 230, 250, 1)",
+    beige: "rgba(245, 245, 220, 1)",
+    mint: "rgba(189, 252, 201, 1)",
+    indigo: "rgba(75, 0, 130, 1)",
+    peach: "rgba(255, 229, 180, 1)",
+    black: "rgba(0, 0, 0, 1)",
+    white: "rgba(255, 255, 255, 1)",
+    gray: "rgba(128, 128, 128, 1)",
+    brown: "rgba(165, 42, 42, 1)"
+};
+// below is a color set in HEX values in case someday we prefer it over RGBA, in which case, we already got it typed out
+ // actually im probably going to update this so they can both be used woooooo - s_d
+// const colors = {
+//    aqua: "#00FFFF",
+//    black: "#000000",
+//    blue: "#0000FF",
+//    fuchsia: "#FF00FF",
+//    gray: "#808080",
+//    green: "#008000",
+//    lime: "#00FF00",
+//    maroon: "#800000",
+//    navy: "#000080",
+//    olive: "#808000",
+//    purple: "#800080",
+//    red: "#FF0000",
+//    silver: "#C0C0C0",
+//    teal: "#008080",
+//    white: "#FFFFFF",
+//    yellow: "#FFFF00",
+//    orange: "#FFA500",
+//    pink: "#FFC0CB",
+//    coral: "#FF7F50",
+//    chocolate: "#D2691E",
+//    indigo: "#4B0082",
+//    violet: "#EE82EE",
+//    gold: "#FFD700",
+//    skyblue: "#87CEEB"
+// };
+
+// export so we can import!
+export default colors;
